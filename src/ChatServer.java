d82873521320ff1615d7019bb59e10f6119814f9
JulHirsch <69107704+JulHirsch@users.noreply.github.com>
2024-06-20 11:52:05 +0200

    create basic broadcasting chatserver

---
+++ src/ChatServer.java
@@ -0,0 +1,56 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ChatServer {
+    private ServerSocket _serverSocket;
+    private List<ClientHandler> _clients;
+
+    public ChatServer(int port) {
+        _clients = new CopyOnWriteArrayList<ClientHandler>(); // thread safe
+
+        try {
+            _serverSocket = new ServerSocket(port);
+            System.out.println("Server started on port " + port);
+
+            while (true) {
+                System.out.println("Waiting for new client...");
+                Socket connectionToClient = _serverSocket.accept();
+                ClientHandler client = new ClientHandler(this, connectionToClient);
+                _clients.add(client);
+                System.out.println("Accepted new client");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if(_serverSocket == null){
+                return;
+            }
+            try {
+                _serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void removeClient(ClientHandler client) {
+        _clients.remove(client);
+    }
+
+    public void broadcastMessage(String message) {
+        System.out.println(message);
+        if (message == null) {
+            return;
+        }
+        for (ClientHandler client : _clients) {
+            client.sendMessage(message);
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer(3141);
+    }
+}
---
+++ src/ClientHandler.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+    private ChatServer _chatServer;
+    private Socket _connectionToClient;
+    private String name;
+    private BufferedReader _fromClientReader;
+    private PrintWriter _toClientWriter;
+
+    public ClientHandler(ChatServer chatServer, Socket connectionToClient) {
+
+        _chatServer = chatServer;
+        _connectionToClient = connectionToClient;
+
+        name = connectionToClient.getInetAddress().getHostAddress(); // set name to ip address
+
+        new Thread(this).start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            _fromClientReader = new BufferedReader(new InputStreamReader(_connectionToClient.getInputStream()));
+            _toClientWriter = new PrintWriter(new OutputStreamWriter(_connectionToClient.getOutputStream()));
+
+            _chatServer.broadcastMessage(name + " connected.");
+            while (true) {
+                String message = _fromClientReader.readLine();//TODO sanitize string
+                _chatServer.broadcastMessage(name + ": " + message);
+            }
+        } catch (IOException e) {
+            _chatServer.removeClient(this);
+            _chatServer.broadcastMessage(name + " disconnected.");
+        } finally {
+            if (_fromClientReader != null) {
+                try {
+                    _fromClientReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (_toClientWriter != null) {
+                _toClientWriter.close();
+            }
+        }
+    }
+
+    public void sendMessage(String message) {
+        _toClientWriter.println(message);
+        _toClientWriter.flush();
+    }
+}
