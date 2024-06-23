ad1ae4ba659064715c64b3e91737972252affde3
JulHirsch <69107704+JulHirsch@users.noreply.github.com>
2024-06-24 00:04:26 +0200

    cleanup: manual imports

--- src/client/ConnectionManager.java
+++ src/client/ConnectionManager.java
@@ -3,7 +3,10 @@ package client;
 import common.Message;
 
 import javax.swing.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class ConnectionManager {
